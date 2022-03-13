f2ec53096f78e8a6030d2002e701d1ff86be8da3
dsrkafuu <dsrkafuu@outlook.com>
2022-03-13 15:31:14 +0800

    fix: id validator

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@ export function minify(str) {
 }
 
 export function validateID(id) {
-  if (!/^[a-z][0-9a-z!@#$%^&*_-]{0,255}$/i.test(id)) {
+  if (!/^[a-z][0-9a-z:!@#$%^&*_-]{0,255}$/i.test(id)) {
     throw new ResError(400, 'Invalid Counter ID');
   }
   return id;
