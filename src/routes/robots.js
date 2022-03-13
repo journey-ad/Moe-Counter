409dbbca3fe5d9a580a5593ea26349c4d9931d7f
dsrkafuu <dsrkafuu@outlook.com>
2022-03-13 21:07:09 +0800

    fix: robots.txt

--- src/routes/robots.js
+++ src/routes/robots.js
@@ -4,8 +4,14 @@ import { genResponse } from '../response';
  * @param {Request} req
  */
 export async function get(req) {
-  const robots = 'User-agent: *\nDisallow: /';
-  return await genResponse(req, robots, {
+  const robots = [
+    'User-agent: *',
+    'Allow: /robots.txt$',
+    'Allow: /favicon.ico$',
+    'Allow: /$',
+    'Disallow: /',
+  ];
+  return await genResponse(req, robots.join('\n'), {
     status: 200,
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
