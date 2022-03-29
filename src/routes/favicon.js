c29659077f405970171cc958da8e4a38fe59323e
dsrkafuu <dsrkafuu@outlook.com>
2022-03-29 17:17:50 +0800

    chore: update favicon

--- src/routes/favicon.js
+++ src/routes/favicon.js
@@ -8,6 +8,6 @@ export async function get(req, event) {
   return await genProxyResponse(
     req,
     event,
-    'https://cdn.jsdelivr.net/gh/dsrkafuu/dsr-assets@3.0.0/favicon/favicon.ico'
+    'https://cdn.jsdelivr.net/gh/dsrkafuu/dsr-assets@9.0.0/favicon/favicon.ico'
   );
 }
